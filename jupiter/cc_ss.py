bbe206aeb07eab7209cbbed7fb154548bef3a5ac
saeedsarkarati <saeedsarkarati@gmail.com>
2023-12-09 07:20:43 -0500

    1402-azar-18

---
+++ jupiter/cc_ss.py
@@ -0,0 +1,124 @@
+#Coupling Coeffitient
+from scipy.special import erf
+import numpy as np
+k0 = 9e9
+e0 = 1 / (4 * np.pi * k0)
+def Iss(x1, x2, y1, y2, z):
+	x = np.abs(x1 - x2)
+	y = np.abs(y1 - y2)
+	z = np.abs(z)
+	I1 = 1 / 12 * (- x**2 - y**2 + 2 * z**2) * ( x**2 + y**2 + z**2) ** (1/2)
+	I2 =  1 / 4 * (y * (x**2 - z**2) ) * np.arcsinh(y / np.hypot(x, z) )
+	I3 =  1 / 4 * (x * (y**2 - z**2) ) * np.arcsinh(x / np.hypot(y, z) )
+	I4 = -1 / 2 * x * y * z * np.arctan(x * y /(z * np.sqrt(x**2 + y**2 + z**2) ) )
+	II = np.sqrt(np.pi) * ( I1 + I2 + I3 + I4 )
+	return II
+def Iss_coplanar(x1, x2, y1, y2):
+	x = np.abs(x1 - x2)
+	y = np.abs(y1 - y2)
+	I1 = 1 / 12 * (- x**2 - y**2) * ( x**2 + y**2) ** (1/2)
+	x0 = (x == 0)
+	y0 = (y == 0)
+	xn = np.logical_not(x0)
+	yn = np.logical_not(y0)
+	I2 = np.zeros_like (x)
+	I3 = np.zeros_like (x)
+	I2[x0] = 0
+	I2[xn] =  1 / 4 * ( y[xn] * x[xn]**2 ) * np.arcsinh( y[xn] / x[xn] )
+	I3[y0] = 0
+	I3[yn] =  1 / 4 * ( x[yn] * y[yn]**2 ) * np.arcsinh( x[yn] / y[yn] )
+	II = np.sqrt(np.pi) * ( I1 + I2 + I3 )
+	return II
+def ISum(Limits, z):
+	s = 0
+	for i in [0, 1]:
+		for j in [0, 1]:
+			for k in [0, 1]:
+				for l in [0, 1]:
+					if (i + j + k + l) % 2 == 0:
+						A = 1
+					else:
+						A = -1
+					s += A * Iss(Limits[0][i], Limits[1][j], Limits[2][k], Limits[3][l], z)
+	return s
+
+def ISum_coplanar(Limits):
+	s = 0
+	for i in [0, 1]:
+		for j in [0, 1]:
+			for k in [0, 1]:
+				for l in [0, 1]:
+					if (i + j + k + l) % 2 == 0:
+						A = 1
+					else:
+						A = -1
+					s += A * Iss_coplanar(Limits[0][i], Limits[1][j], Limits[2][k], Limits[3][l])
+	return s
+
+def saeed(Lx ,Ly,x0,y0,z):
+	x1L = [x0-Lx/2, x0+Lx/2]
+	x2L = [-Lx/2, +Lx/2]
+	y1L = [y0-Ly/2, y0+Ly/2]
+	y2L = [-Ly/2, +Ly/2]
+	Limits = [x1L, x2L, y1L, y2L]
+	return ISum(Limits, z) * k0 / (Lx*Ly)**2 *2 /np.sqrt(np.pi)
+def saeed_coplanar(Lx ,Ly,x0,y0):
+	x1L = [x0-Lx/2, x0+Lx/2]
+	x2L = [-Lx/2, +Lx/2]
+	y1L = [y0-Ly/2, y0+Ly/2]
+	y2L = [-Ly/2, +Ly/2]
+	Limits = [x1L, x2L, y1L, y2L]
+	return ISum_coplanar(Limits) * k0 / (Lx*Ly)**2 *2 /np.sqrt(np.pi)
+def saeed_sc(x ,y):
+	II = 1 / 3 * (x**3 + y**3) + 1 / 3 * (-x**2 - y**2)*np.hypot(x, y) + \
+	     y * x**2 * np.arcsinh (y / x) + x * y**2 * np.arcsinh (x / y)
+	return II * k0 / (x*y)**2 *2 
+def hitoshi_i(x ,y, d):
+	r = np.sqrt(d*d + x*x +y * y)
+	I1 = -d * np.arctan(x * y /(d * r))
+	I2 = y * np.log(x + r)
+	I3 = x * np.log(y + r)
+	return I1 + I2 + I3
+def hitoshi_coplanar_i(x ,y):
+	r = np.hypot(x, y)
+	I = x * np.log(r + y) + y * np.log(r + x)
+	return I
+def hitoshi(Lx ,Ly,x0,y0,z):
+	I = hitoshi_i(x0+Lx/2, y0+Ly/2, z) - hitoshi_i(x0+Lx/2, y0-Ly/2, z)
+	J = hitoshi_i(x0-Lx/2, y0-Ly/2, z) - hitoshi_i(x0-Lx/2, y0+Ly/2, z)
+	return (I+J)/(Lx*Ly) * k0
+def hitoshi_coplanar(Lx ,Ly,x0,y0):
+	I = hitoshi_coplanar_i(x0+Lx/2, y0+Ly/2) - hitoshi_coplanar_i(x0+Lx/2, y0-Ly/2)
+	J = hitoshi_coplanar_i(x0-Lx/2, y0-Ly/2) - hitoshi_coplanar_i(x0-Lx/2, y0+Ly/2)
+	return (I+J)/(Lx*Ly)*k0
+def orion(Lx, Ly):
+	II =  1/Lx * np.arcsinh(Lx / Ly) + 1/Ly * np.arcsinh(Ly / Lx) + \
+	(Lx / Ly**2 + Ly / Lx**2 - (1 / Lx**2 + 1 / Ly**2) * np.hypot(Lx,Ly) )/3
+	return II * 2 * k0
+def zho(Lx ,Ly,x0,y0,z):
+	return 1/np.sqrt(x0**2 + y0**2 + z**2)*k0 
+def zho_coplanar(Lx ,Ly,x0,y0):
+	return 1/np.sqrt(x0**2 + y0**2)*k0 	
+if __name__ == "__main__":
+	d = 0
+	Lx = 1
+	Ly = 1
+	x1L = [d-Lx/2, d+Lx/2]
+	x2L = [-Lx/2, +Lx/2]
+	y1L = [-Ly/2, +Ly/2]
+	y2L = [-Ly/2, +Ly/2]
+	Limits = [x1L, x2L, y1L, y2L]
+	z = 1e-9
+	Io = orion(Lx, Ly)
+	print ('orion %e' %Io)
+	Isc = saeed_sc(Lx, Ly)
+	print ('saeed self copling %e' %Isc, 'saeed_sc/orion',Isc / Io)
+
+	Icp = saeed_coplanar(Lx, Ly, d, 0)
+	print ('saeed coplanar %e' %Icp, 'saeed/orion',Icp / Io)
+
+	Is = saeed(Lx, Ly, d, 0, z)
+	print ('saeed %e' %Is, 'saeed/orion',Is / Io)
+
+	Ih = hitoshi(Lx, Ly, d, 0, z)
+	print ('hitoshi',Ih, 'saeed/hitoshi',Is/Ih)
---
+++ jupiter/perpendicular.py
@@ -0,0 +1,68 @@
+#Coupling Coeffitient
+from cc_ss import *
+from scipy.special import erf
+import numpy as np
+k0 = 9e9
+e0 = 1 / (4 * np.pi * k0)
+def Kss(x1, y1, z1):
+	x = np.float64(x1)
+	y = np.float64(y1)
+	z = np.float64(z1)
+	# ~ if type(x) == type(1.5):
+		# ~ x = np.array([x])
+		# ~ y = np.array([y])
+		# ~ z = np.array([z])
+	R = np.sqrt(x**2 + y**2 + z**2)
+	K0 = -(y * z* R)/6
+#  Condition of 1
+	K1 = np.zeros_like (x)
+	C1 = np.logical_or (x !=0 , z !=0)
+	K1[C1] = -(z[C1]*(-3*x[C1]**2 + z[C1]**2)*np.arcsinh(y[C1]/np.hypot(x[C1], z[C1])))/12.
+
+	K2 = np.zeros_like (x)
+	C2 = np.logical_or (x !=0 , y !=0)
+	K2[C2] = -(y[C2]*(-3*x[C2]**2 + y[C2]**2)*np.arcsinh(z[C2]/np.hypot(x[C2], y[C2])))/12.
+	K3 = np.zeros_like (x)
+	C3 = np.logical_or (y !=0 , z !=0)
+	K3[C3] = (x[C3]*y[C3]*z[C3]*np.arcsinh(x[C3]/np.hypot(y[C3], z[C3])))/2.
+	K4 = np.zeros_like (x)
+	C4 = z !=0 
+	K4[C4] = -(x[C4]*z[C4]**2*np.arctan(x[C4] * y[C4] / (z[C4] * R[C4] )))/4.
+	K5 = np.zeros_like (x)
+	C5 = y !=0 
+	K5[C5] = -x[C5]*y[C5]**2*np.arctan( x[C5] * z[C5] / (y[C5] * R[C5] ) )/4.
+	K6 = np.zeros_like (x)
+	C6 = x !=0 
+	K6[C6] = -x[C6]**3*np.arctan( y[C6] * z[C6] / (x[C6] * R[C6] ) ) /12.
+	# ~ ---------------------
+	II =  np.sqrt(np.pi) * (K0 + K1 + K2 + K3 + K4 + K5 + K6 )
+	return II
+def KSum(Limits):
+	y, z = Limits[4]
+	s = 0
+	for i in [0, 1]:
+		for j in [0, 1]:
+			for k in [0, 1]:
+				for l in [0, 1]:
+					if (i + j + k + l) % 2 == 0:
+						A = 1
+					else:
+						A = -1
+					s += A * Kss(Limits[0][i] - Limits[1][j], Limits[2][k] - y, \
+								 Limits[3][l] - z)
+	return s
+	
+def perpendicular(Lx ,Ly,x0,y0,z0):
+
+	L = Lx
+	x1 = [x0-L/2, x0+L/2]
+	x2 = [-L/2, L/2]
+	y1 = [y0-L/2, y0+L/2]
+	z1 = [z0-L/2, z0+L/2]
+	z = 0
+	y = 0
+	Limits = [x1, x2, y1, z1,[y, z]]
+	return -KSum(Limits) * k0 / (L*L)**2  *2 /np.sqrt(np.pi)
+if __name__ == "__main__":
+	print ("%e" %saeed (2, 2, 0, 0 , 1))
+	print ("%e" %perpendicular())
